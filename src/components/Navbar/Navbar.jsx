be039631ae4bfa7bd5b23b19de914b4a516b8368
Samson Cheung <samsonc89@yahoo.com>
2023-07-11 11:44:15 -0700

    Add logo to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import "./navbar.css";
+import logo from "../../assets/logo.png";
 import NavLinks from "./NavLinks";
 
 const Navbar = () => {
   return (
     <nav className="nav-wrapper container">
-      <div className="nav-logo-wrapper"></div>
+      <div className="nav-logo-wrapper">
+        <img src={logo} alt="" />
+      </div>
       <div className="nav-items">
         <NavLinks />
       </div>
