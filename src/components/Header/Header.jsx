8873ad0481567bc9e80852c796e0a15ef3ef5c00
Samson Cheung <samsonc89@yahoo.com>
2023-07-11 16:18:05 -0700

    Add hero section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
 import "./header.css";
 
 const Header = () => {
-  return <header className="header-wrapper container">Header</header>;
+  return (
+    <header className="header-wrapper container">
+      <div className="hero-container"></div>
+      <div className="hero-text">
+        <h1 className="light-text">Reliable kidcare on demand</h1>
+        <p className="light-text">
+          Otter matches parents who need care with trusted sitters in their
+          community, on-demand.
+        </p>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
