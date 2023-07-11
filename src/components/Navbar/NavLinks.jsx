e0af7791784dd0517b7c0f4c1b0cf19dd724f49c
Samson Cheung <samsonc89@yahoo.com>
2023-07-11 11:06:21 -0700

    Rename folder

---
+++ src/components/Navbar/NavLinks.jsx
@@ -0,0 +1,20 @@
+const NavLinks = () => {
+  return (
+    <>
+      <li>
+        <a href="#parents">Parents</a>
+      </li>
+      <li>
+        <a href="#sitters">Sitters</a>
+      </li>
+      <li>
+        <a href="#trust">Trust & Safety</a>
+      </li>
+      <li>
+        <a href="#blog">Blog</a>
+      </li>
+    </>
+  );
+};
+
+export default NavLinks;
