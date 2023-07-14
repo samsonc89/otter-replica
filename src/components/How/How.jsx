51986c15501419586804472d628f4116ef1ddf8f
Samson Cheung <samsonc89@yahoo.com>
2023-07-14 14:12:46 -0700

    Add tab content

--- src/components/How/How.jsx
+++ src/components/How/How.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import "./how.css";
+import tab3 from "../../assets/tab3.jpg";
 
 const How = () => {
   return (
@@ -12,18 +13,37 @@ const How = () => {
               We help families find reliable, flexible kidcare
             </h2>
             <p className="main-text">
-              We'll match you with sitters based on your family's needs and
-              their availability.
+              We&apos;ll match you with sitters based on your family&apos;s
+              needs and their availability.
             </p>
           </div>
-          <div className="how-tabs-wrapper">
-            <div className="how-tab">Sign up</div>
-            <div className="how-tab">Book a sitter</div>
-            <div className="how-tab">Get care & pay</div>
-          </div>
+          {/* <div className="how-tabs-wrapper"> */}
+          <div className="how-tab">Sign up</div>
+          <div className="how-tab">Book a sitter</div>
+          <div className="how-tab">Get care & pay</div>
+          {/* </div> */}
         </div>
         <div className="how-column how-right">
-          <div className="how-image-wrapper"></div>
+          <div className="how-tab-content-wrapper active-tab">
+            <div className="how--tab-image-wrapper">
+              <img src={tab3} className="tab-image" />
+            </div>
+            <div className="how-tab-text-wrapper">
+              <p>
+                We like to keep it simple. Tell us your name, contact
+                information, and a little bit about you and your kids. Just like
+                that, you're all set.
+              </p>
+            </div>
+          </div>
+          <div className="how-tab-content-wrapper">
+            <div className="how-image-wrapper"></div>
+            <div className="how-text-wrapper"></div>
+          </div>
+          <div className="how-tab-content-wrapper">
+            <div className="how-image-wrapper"></div>
+            <div className="how-text-wrapper"></div>
+          </div>
         </div>
       </div>
     </section>
