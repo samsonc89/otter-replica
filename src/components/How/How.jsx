e1a71b64dcfbab31abc0210e1297c217ee27b213
Samson Cheung <samsonc89@yahoo.com>
2023-07-14 10:34:15 -0700

    Add text to how section

--- src/components/How/How.jsx
+++ src/components/How/How.jsx
@@ -1,7 +1,27 @@
 import "./how.css";
 
 const How = () => {
-  return <></>;
+  return (
+    <section className="main-section">
+      <div className="how-wrapper">
+        <div className="how-column how-left">
+          <div className="how-text-wrapper">
+            <p className="tiny">how it works</p>
+            <h2 className="how-heading">
+              We help families find reliable, flexible kidcare
+            </h2>
+            <p className="main-text">
+              We'll match you with sitters based on your family's needs and
+              their availability.
+            </p>
+          </div>
+        </div>
+        <div className="how-column how-right">
+          <div className="how-image-wrapper"></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default How;
