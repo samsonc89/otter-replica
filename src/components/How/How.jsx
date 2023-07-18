36daa3d9a7b75aa9f91fabd5fdaee613ab3169be
Samson Cheung <samsonc89@yahoo.com>
2023-07-18 11:21:25 -0700

    Remove commented out placeholder data

--- src/components/How/How.jsx
+++ src/components/How/How.jsx
@@ -26,21 +26,6 @@ const How = () => {
       text: "We've got it from here. Your sitter will show up so you can head out. After your booking wraps up you'll receive a payment request through our app.",
     },
   ];
-  //   const tab1 = {
-  //     title: "Sign up",
-  //     img: tabOneImg,
-  //     text: "We like to keep it simple. Tell us your name, contact information, and a little bit about you and your kids. Just like that, you're all set.",
-  //   };
-  //   const tab2 = {
-  //     title: "Book a sitter",
-  //     img: tabTwoImg,
-  //     text: "Let us know when you need care and we'll match you with a sitter based on your family's needs and their availability.",
-  //   };
-  //   const tab3 = {
-  //     title: "Get care & pay",
-  //     img: tabThreeImg,
-  //     text: "We've got it from here. Your sitter will show up so you can head out. After your booking wraps up you'll receive a payment request through our app.",
-  //   };
 
   return (
     <section className="main-section">
@@ -71,42 +56,11 @@ const How = () => {
               );
             })}
           </div>
-          {/* <div className="how-tabs-wrapper">
-            <div className="tab-item">
-              <a href="#" className="how-tab">
-                Sign up
-              </a>
-              <TabCard
-                //   extraClass="narrow-content"
-                props={tab1}
-              />
-            </div>
-            <div className="tab-item">
-              <a href="#" className="how-tab">
-                Book a sitter
-              </a>
-              <TabCard
-                //   extraClass="narrow-content"
-                props={tab2}
-              />
-            </div>
-            <div className="tab-item">
-              <a href="#" className="how-tab">
-                Get care & pay
-              </a>
-              <TabCard extraClass="narrow-content" props={tab3} />
-            </div>
-          </div> */}
         </div>
         <div className="how-column how-right">
           {tabData.map((tab, i) => {
-            // eslint-disable-next-line react/jsx-key
-
             return selected === i ? <TabCard key={i} props={tab} /> : "";
           })}
-          {/* <TabCard extraClass="active-content" props={tab1} />
-          <TabCard props={tab2} />
-          <TabCard props={tab3} /> */}
         </div>
       </div>
     </section>
