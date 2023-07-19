c1308dd7fd79b9740080b8ebc6d8df59eada2c8f
Samson Cheung <samsonc89@yahoo.com>
2023-07-19 10:36:16 -0700

    Create styles for testimonial card

--- src/components/TestimonialCard/TestimonialCard.jsx
+++ src/components/TestimonialCard/TestimonialCard.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/prop-types */
+import "./testimonialCard.css";
+
 const TestimonialCard = ({ props }) => {
   return (
     <>
       <div className="testimonial-card">
-        <p className="testimonial-text">{props.text}</p>
+        <p className="testimonial-text">"{props.text}"</p>
         <div className="testimonial-label-wrapper">
           <p className="testimonial-name">{props.name},</p>
           <p className="testimonial-type">{props.type}</p>
