5830e6076d8ebfad8f42550259217be7c1d27c58
Samson Cheung <samsonc89@yahoo.com>
2023-07-19 11:26:00 -0700

    Change testimonial card alignment

--- src/components/TestimonialCard/TestimonialCard.jsx
+++ src/components/TestimonialCard/TestimonialCard.jsx
@@ -7,10 +7,9 @@ const TestimonialCard = ({ props }) => {
     <>
       <div className="testimonial-card">
         <p className="testimonial-text">"{props.text}"</p>
-        <div className="testimonial-label-wrapper">
-          <p className="testimonial-name">{props.name},</p>
-          <p className="testimonial-type">{props.type}</p>
-        </div>
+        <p className="testimonial-label">
+          {props.name}, {props.type}
+        </p>
       </div>
     </>
   );
