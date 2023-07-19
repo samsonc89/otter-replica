e28639536a6bb67b5d526611cb7305107325c47f
Samson Cheung <samsonc89@yahoo.com>
2023-07-18 17:20:21 -0700

    Create containers folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Header } from "./Header/Header";
-export { default as How } from "./How/How";
+export { default as How } from "../containers/How/How";
