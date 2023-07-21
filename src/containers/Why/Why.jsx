18e2718f337516dd5d34a87602b6d1f186c5bd74
Samson Cheung <samsonc89@yahoo.com>
2023-07-21 15:01:17 -0700

    Create component for cards in why container

--- src/containers/Why/Why.jsx
+++ src/containers/Why/Why.jsx
@@ -1,5 +1,21 @@
+/* eslint-disable react/prop-types */
 import { ContentHeading } from "../../components";
 import "./why.css";
+
+const WhyCard = ({ props, extraClasses }) => {
+  return (
+    <>
+      {" "}
+      <div className={`why-card ${extraClasses}`}>
+        <div className="why-card-text-wrapper">
+          <h2 className="why-card-heading">{props.heading}</h2>
+          <p className="why-card-text">{props.text}</p>
+        </div>
+        <div className="why-card-image"></div>
+      </div>
+    </>
+  );
+};
 
 const Why = () => {
   const sectionCopy = {
@@ -7,21 +23,31 @@ const Why = () => {
     title: "We pop in so you can pop out",
     text: "Making the impossible things about petcare possible.",
   };
+
+  const cards = [
+    {
+      heading: "Looking Ahead",
+      text: "Look at you being so ahead of the game. We can book childcare up to a month out — we look forward to seeing you then.",
+    },
+    {
+      heading: "A Night Out",
+      text: "Nothing should get in the way of the time you spend together, without your kids. Our sitters make date night easy to pull off.",
+    },
+    {
+      heading: "In a Pinch",
+      text: "Emergency? Sick nanny? No problem, we can help! We can fill requests with as little as two hours' notice.",
+    },
+  ];
+
   return (
     <section className="main-section why-section">
       <ContentHeading classes={"why-text-wrapper"} props={sectionCopy} />
       <div className="why-row1">
-        <div className="why-card1">
-          <div className="why-card-text"></div>
-          <div className="why-card-image"></div>
-        </div>
-        <div className="why-card2">
-          <div className="why-card-text"></div>
-          <div className="why-card-image"></div>
-        </div>
+        <WhyCard props={cards[0]} />
+        <WhyCard props={cards[1]} />
       </div>
       <div className="why-row2">
-        <div className="why-row2-text"></div>
+        <WhyCard props={cards[2]} extraClasses={"why-row2-card"} />
         <div className="why-row2-images">
           <div className="why-row2-image1"></div>
           <div className="why-row2-image2"></div>
