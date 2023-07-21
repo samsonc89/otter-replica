da20d2e280d116dda4ed749a97e444f88e72b843
Samson Cheung <samsonc89@yahoo.com>
2023-07-21 14:00:16 -0700

    Add section content heading

--- src/containers/Why/Why.jsx
+++ src/containers/Why/Why.jsx
@@ -1,7 +1,17 @@
+import { ContentHeading } from "../../components";
 import "./why.css";
 
 const Why = () => {
-  return <section className="main-section why-section">Why</section>;
+  const sectionCopy = {
+    smallTitle: "Why Sitter?",
+    title: "We pop in so you can pop out",
+    text: "Making the impossible things about petcare possible.",
+  };
+  return (
+    <section className="main-section why-section">
+      <ContentHeading classes={"why-text-wrapper"} props={sectionCopy} />
+    </section>
+  );
 };
 
 export default Why;
