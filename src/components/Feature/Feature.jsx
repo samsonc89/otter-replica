60817e38f824032031e0e9febc4b28be1e6cd1b9
Samson Cheung <samsonc89@yahoo.com>
2023-07-21 11:47:47 -0700

    Add company logos

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,11 +1,28 @@
 import "./feature.css";
+import forbes from "../../assets/forbes.svg";
+import NYT from "../../assets/NYT.svg";
+import mom from "../../assets/mom.svg";
+import parents from "../../assets/parents.svg";
 
 const Feature = () => {
   return (
     <section className="main-section featured-section">
       <div className="featured-content">
         <p className="small-heading">As featured in</p>
-        <div className="companies-wrapper"></div>
+        <div className="companies-wrapper">
+          <div className="company">
+            <img src={forbes} />
+          </div>
+          <div className="company">
+            <img src={parents} />
+          </div>
+          <div className="company">
+            <img src={NYT} />
+          </div>
+          <div className="company">
+            <img src={mom} />
+          </div>
+        </div>
       </div>
     </section>
   );
