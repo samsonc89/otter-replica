e4a478161380ca393c5c9e1af5f9ca9d05ac5b41
Samson Cheung <samsonc89@yahoo.com>
2023-07-28 17:14:31 -0700

    Add components for home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import { Feature, Navbar } from "./components";
+import {
+  Footer,
+  Header,
+  How,
+  Questions,
+  Testimonials,
+  Why,
+} from "./containers";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <main>
+        <How />
+        <Testimonials />
+        <Feature />
+        <Why />
+        <Questions />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
