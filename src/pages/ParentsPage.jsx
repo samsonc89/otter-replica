f00781fbc074da933d4535179175cf499a80053e
Samson Cheung <samsonc89@yahoo.com>
2023-08-01 12:52:38 -0700

    Add heading copy to parents page

--- src/pages/ParentsPage.jsx
+++ src/pages/ParentsPage.jsx
@@ -3,6 +3,10 @@ import { How } from "../containers";
 import MainLayout from "../layouts/MainLayout";
 
 function Parents() {
+  const headingCopy = {
+    heading: "Quality petcare, when you need it",
+    text: "Sitter's care options are designed to cater to both planned and unplanned schedule changes, with sitters available with as little as 2 hours notice.",
+  };
   return (
     <>
       <MainLayout>
