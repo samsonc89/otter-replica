9f73e53551317397a08977b4774f543f39e3e21e
Samson Cheung <samsonc89@yahoo.com>
2023-08-01 11:31:29 -0700

    Add main app to route

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/otter-replica",
-    element: <div>Hello world!</div>,
+    element: <App />,
   },
 ]);
 
