8eef7d33bfbc3dbabc1f9f9e7aca8e637dbb8f81
Samson Cheung <samsonc89@yahoo.com>
2023-08-01 12:25:50 -0700

    Fix path for footer

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,4 +3,3 @@ export { default as Header } from "./Header/Header";
 export { default as Testimonials } from "./Testimonials/Testimonials";
 export { default as Why } from "./Why/Why";
 export { default as Questions } from "./Questions/Questions";
-export { default as Footer } from "./Footer/Footer";
