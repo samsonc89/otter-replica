68686390fd1cffc8683f03e043bff51746740ba9
Samson Cheung <samsonc89@yahoo.com>
2023-08-02 15:17:37 -0700

    Separate header items in divs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,12 @@ import "./header.css";
 const Header = ({ props }) => {
   return (
     <header className="main-section header-section">
-      <h1>{props.heading}</h1>
-      <p className="heading-text">{props.text}</p>
+      <div className="header-text-wrapper">
+        <h1>{props.heading}</h1>
+        <p className="heading-text">{props.text}</p>
+        <button className="btn-signup-inverse">{props.btnText}</button>
+      </div>
+      <div className="header-img-wrapper"></div>
     </header>
   );
 };
