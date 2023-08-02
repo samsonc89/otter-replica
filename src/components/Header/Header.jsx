64a25cf37f82b14fbdf9d77d33a3f44c47119432
Samson Cheung <samsonc89@yahoo.com>
2023-08-02 15:14:46 -0700

    Add props for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import "./header.css";
 
-const Header = () => {
-  return <header className="main-section header-section">Header</header>;
+const Header = ({ props }) => {
+  return (
+    <header className="main-section header-section">
+      <h1>{props.heading}</h1>
+      <p className="heading-text">{props.text}</p>
+    </header>
+  );
 };
 
 export default Header;
