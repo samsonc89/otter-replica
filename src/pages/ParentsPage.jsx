3e3c94e97d852cd38d26104942166edbbba166a0
Samson Cheung <samsonc89@yahoo.com>
2023-08-02 15:14:09 -0700

    Update btn text copy

--- src/pages/ParentsPage.jsx
+++ src/pages/ParentsPage.jsx
@@ -6,11 +6,12 @@ function Parents() {
   const headingCopy = {
     heading: "Quality petcare, when you need it",
     text: "Sitter's care options are designed to cater to both planned and unplanned schedule changes, with sitters available with as little as 2 hours notice.",
+    btnText: "Book petcare",
   };
   return (
     <>
       <MainLayout>
-        <Header />
+        <Header props={headingCopy} />
         <How />
       </MainLayout>
     </>
