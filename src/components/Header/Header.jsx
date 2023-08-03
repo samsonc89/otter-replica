9841b7bec03a68ada1fb6a07f8fb934d08b8c215
Samson Cheung <samsonc89@yahoo.com>
2023-08-02 17:25:14 -0700

    Add divs to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,20 @@
+/* eslint-disable react/prop-types */
 import "./header.css";
 
 const Header = ({ props }) => {
   return (
     <header className="main-section header-section">
-      <div className="header-text-wrapper">
+      <div className="header-col=left">
         <h1>{props.heading}</h1>
         <p className="heading-text">{props.text}</p>
         <button className="btn-signup-inverse">{props.btnText}</button>
       </div>
-      <div className="header-img-wrapper"></div>
+      <div className="header-col-right">
+        <div className="header-image-wrapper">
+          <img src={props.img} alt="header-image" />
+        </div>
+        <div className="header-shape"></div>
+      </div>
     </header>
   );
 };
