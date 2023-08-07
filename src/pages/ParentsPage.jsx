d8ba21897be15af319aa1c79cb56265e4804f765
Samson Cheung <samsonc89@yahoo.com>
2023-08-07 11:02:34 -0700

    Add header shape paths

--- src/pages/ParentsPage.jsx
+++ src/pages/ParentsPage.jsx
@@ -1,12 +1,17 @@
 import { Header } from "../components";
 import { How } from "../containers";
 import MainLayout from "../layouts/MainLayout";
+import image from "../assets/header-parents.jpg";
 
 function Parents() {
   const headingContent = {
     heading: "Quality petcare, when you need it",
     text: "Sitter's care options are designed to cater to both planned and unplanned schedule changes, with sitters available with as little as 2 hours notice.",
     btnText: "Book petcare",
+    img: image,
+    topShape: "middle-left",
+    bottomShape: "bottom-left",
+    bottomColor: "#94954c",
   };
   return (
     <>
