ebcc5ea2fc8fdd903f32290471ab6ffd72005f0b
Samson Cheung <samsonc89@yahoo.com>
2023-08-10 11:58:11 -0700

    Make sitters page

---
+++ src/pages/SittersPage.jsx
@@ -0,0 +1,32 @@
+import { CTA, Header } from "../components";
+import { How, Testimonials } from "../containers";
+import MainLayout from "../layouts/MainLayout";
+import image from "../assets/header-parents.jpg";
+import ctaImage from "../assets/cta-parents.webp";
+
+function Sitters() {
+  const content = {
+    heading: "Quality petcare, when you need it",
+    text: "Sitter's care options are designed to cater to both planned and unplanned schedule changes, with sitters available with as little as 2 hours notice.",
+    btnText: "Book petcare",
+    img: image,
+    topShape: "middle-left",
+    bottomShape: "bottom-left",
+    bottomColor: "#94954c",
+    ctaHeading: "Experienced petcare is right around the corner.",
+    ctaBtn: "Book petcare",
+    ctaImg: ctaImage,
+  };
+  return (
+    <>
+      <MainLayout>
+        <Header props={content} />
+        <How />
+        <CTA props={content} />
+        <Testimonials filter={"Parent"} />
+      </MainLayout>
+    </>
+  );
+}
+
+export default Sitters;
